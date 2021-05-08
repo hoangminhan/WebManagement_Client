0569695f0b2081390b1819f4998a5fa4e9452c2b
hoangminhan <hoangminhan13g@gmail.com>
2021-05-08 23:00:10 +0700

    update infor

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,30 +1,29 @@
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const About = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({
-      type: 'SET_TITLE',
-      payload: 'Thông tin'
-    })
-  })
+      type: "SET_TITLE",
+      payload: "Thông tin",
+    });
+  });
 
   return (
-    <div id='about-tab'>
-      <div className='about-tab-container'>
-        <h1>Bùi Văn Mạnh</h1>
-        <h4>17/11/2001</h4>
-        <h4>Khoa Học Máy Tính 2 - K14</h4>
-        <h4>Đại Học Công Nghiệp Hà Nội - FIT - HaUI</h4>
-        <h1 style={{color: 'blue', margin: 32}}>
-          ĐỒ ÁN TỐT NGHIỆP: PHẦN MỀM QUẢN LÝ
+    <div id="about-tab">
+      <div className="about-tab-container">
+        <h1>Hoàng Minh An</h1>
+        <h4>17/02/1999</h4>
+        <h4>DHCNTT13B-17092841</h4>
+        <h4>Đại Học Công Nghiệp</h4>
+        <h1 style={{ color: "blue", margin: 32 }}>
+          ĐỒ ÁN TỐT NGHIỆP: Quản lý thông tin khách hàng trên nền tảng big data
         </h1>
       </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default About
+export default About;
